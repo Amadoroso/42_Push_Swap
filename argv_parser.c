117ba15c75e63211200b41398a9b4b747ad0285a
Andre Benegas Pinho Amador <apinho-a@c1r6s10.42lisboa.com>
2026-06-02 20:32:44 +0100

    modified atoi and libft.h, creating argument parsing structure

--- argv_parser.c
+++ argv_parser.c
@@ -6,29 +6,52 @@
 /*   By: apinho-a <apinho-a@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/02 18:37:35 by apinho-a          #+#    #+#             */
-/*   Updated: 2026/06/02 19:21:01 by apinho-a         ###   ########.fr       */
+/*   Updated: 2026/06/02 20:32:05 by apinho-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	ft_error_checker(int argc, char **argv)
+void ft_stack_clear(t_int_node **stack_head)
+{
+	t_int_node	*next;
+
+	if (!stack_head || !*stack_head)
+		return ;
+	while (*stack_head != NULL)
+	{
+		next = (*stack_head)->next;
+		free(*stack_head);
+		*stack_head = next;
+	}
+	free (stack_head);
+	stack_head = NULL;
+}
+
+void ft_stack_creator(t_int_node **stack_head, int atoi_result)
+{
+	
+}
+
+int	ft_argv_parser(int argc, char **argv)
 {
 	int	*atoi_check;
 	int	atoi_result;
+	t_int_node *a_stack;
 	
 	if (argc <= 1)
-		return (1);
+		return (-1);
+	a_stack = ft_calloc(ft_strlen(argv) + 1, sizeof(t_int_node));
 	while (*argv != 0)
 	{
-		atoi_result = ft_atoi(*argv);
-		if (*atoi_check)
-			ft_stack_a_creator();
+		atoi_result = ft_atoi(*argv, atoi_check);
+		if (*atoi_check == -1)
+			return(ft_stack_clear(a_stack), -1);
+		else
+			ft_stack_creator(a_stack, atoi_result);
 		argv++;
 	}
 	return (0);
 }
-t_int_node *ft_stack_a_creator(int atoi_result)
-{
-	
-}
+
+
--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: apinho-a <apinho-a@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/01 21:32:52 by apinho-a          #+#    #+#             */
-/*   Updated: 2026/06/02 19:07:57 by apinho-a         ###   ########.fr       */
+/*   Updated: 2026/06/02 19:47:38 by apinho-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,6 @@
 
 int	main(int argc, char **argv)
 {
-	if (ft_error_checker(argc, argv) == 1)
+	if (ft_argv_parser(argc, argv) == -1)
 		return (ft_putstr_fd("Error\n", 2), 1);
 }
