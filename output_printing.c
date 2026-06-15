95ebbe238ea2d8fd81508f9e78d65d3f158f1c72
Andre Benegas Pinho Amador <apinho-a@c1r6s11.42lisboa.com>
2026-06-15 18:34:11 +0100

    printing bug fixed. Printing behaviour is complete

--- output_printing.c
+++ output_printing.c
@@ -6,7 +6,7 @@
 /*   By: apinho-a <apinho-a@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/08 11:29:12 by apinho-a          #+#    #+#             */
-/*   Updated: 2026/06/15 18:29:59 by apinho-a         ###   ########.fr       */
+/*   Updated: 2026/06/15 18:33:48 by apinho-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,7 @@
 void	ft_info_filler(t_stack **stack_head, t_info **info, char *op)
 {
 	if (!(*stack_head)->bench)
-	{
-		ft_printf("%s\n", op);
-		return ;
-	}
+		return (ft_printf("%s\n", op), NULL);
 	if (!strcmp(op, "sa"))
 		(*info)->sa++;
 	if (!strcmp(op, "sb"))
@@ -38,6 +35,7 @@ void	ft_info_filler(t_stack **stack_head, t_info **info, char *op)
 		(*info)->rrb++;
 	if (!strcmp(op, "rrr"))
 		(*info)->rrr++;
+	(*info)->total_ops++;
 }
 // Prints output with more detail (stack_head->bench = 1)
 void	ft_bench_printer(t_info **info, t_stack **stack_head)
